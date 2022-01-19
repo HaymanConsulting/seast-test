142bc4ce2d7b66592f827de4be677cfcf341001d
Gregory Hayman <hayman.gregory@gmail.com>
2022-01-18 19:32:17 -0700

    Updated runtests for periodic and additional SEMI tests

--- runtests.bat
+++ runtests.bat
@@ -1,4 +1,4 @@
-GOTO ORIG_NBODYMOD
+GOTO CUR_PERIODIC
 
 :CUR_FIRST
 ECHO "Running current code base, first-order tests"
@@ -8,23 +8,27 @@ chdir "../HDSim"
 "../../bin/HydroDynDriver_x64.exe" "./hd_driver.inp"
 chdir "../FASTSim"
 "../../bin/openfast_x64.exe" "./fast_driver.fst"
+"../../bin/openfast_x64.exe" "./fast_driver_fixed_no_offset.fst"
+"../../bin/openfast_x64.exe" "./fast_driver_fixed_w_surge.fst"
 chdir "../../.."  REM move back up to top-level folder
 GOTO FINISH
 
 :CUR_SECOND
 ECHO "Running current code base, first-order + second-order tests"
-chdir "./current/"5MW_OC4Semi-second/SeaStSim"
+chdir "./current/5MW_OC4Semi-second/SeaStSim"
 "../../bin/SeaStateDriver_x64.exe" "./seastate_driver.inp"
 chdir "../HDSim"
 "../../bin/HydroDynDriver_x64.exe" "./hd_driver.inp"
 chdir "../FASTSim"
 "../../bin/openfast_x64.exe" "./fast_driver.fst"
+"../../bin/openfast_x64.exe" "./fast_driver_fixed_no_offset.fst"
+"../../bin/openfast_x64.exe" "./fast_driver_fixed_w_surge.fst"
 chdir "../../.."  REM move back up to top-level folder
 GOTO FINISH
 
 :ORIG_FIRST
 ECHO "Running original code base, first-order tests"
-chdir "./original/"5MW_OC4Semi-firstonly/HDSim"
+chdir "./original/5MW_OC4Semi-firstonly/HDSim"
 "../../bin/HydroDynDriver_x64.exe" "./hd_driver.inp"
 chdir "../FASTSim"
 "../../bin/openfast_x64.exe" "./fast_driver.fst"
@@ -33,16 +37,25 @@ GOTO FINISH
 
 :ORIG_SECOND
 ECHO "Running original code base, first-order + second-order tests"
-chdir "./original/"5MW_OC4Semi-second/HDSim"
+chdir "./original/5MW_OC4Semi-second/HDSim"
 "../../bin/HydroDynDriver_x64.exe" "./hd_driver.inp"
 chdir "../FASTSim"
 "../../bin/openfast_x64.exe" "./fast_driver.fst"
 chdir "../../.."  REM move back up to top-level folder
 GOTO FINISH
 
+:CUR_PERIODIC
+ECHO "Running original code base, periodic waves tests"
+chdir "./current/Periodic/HDSim"
+"../../bin/HydroDynDriver_x64.exe" "./hd_driver_zero_disp.inp"
+"../../bin/HydroDynDriver_x64.exe" "./hd_driver_7m_disp.inp"
+"../../bin/HydroDynDriver_x64.exe" "./hd_driver_14m_disp.inp"
+chdir "../.."  REM move back up to top-level folder
+GOTO FINISH
+
 :CUR_NBODYMOD
 ECHO "Running current code base, NBodyMod tests"
-chdir "./current/"NBodyMod1/
+chdir "./current/NBodyMod1/"
 "../bin/HydroDynDriver_x64.exe" "./NBodyMod1.dvr"
 cd "../NBodyMod2"
 "../bin/HydroDynDriver_x64.exe" "./NBodyMod2.dvr"
@@ -53,7 +66,7 @@ GOTO FINISH
 
 :ORIG_NBODYMOD
 ECHO "Running original code base, NBodyMod tests"
-chdir "./original/"NBodyMod1/
+chdir "./original/NBodyMod1/"
 "../bin/HydroDynDriver_x64.exe" "./NBodyMod1.dvr"
 cd "../NBodyMod2"
 "../bin/HydroDynDriver_x64.exe" "./NBodyMod2.dvr"
